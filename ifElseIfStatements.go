9942ec937ea89a935906e925186dae506ff7915d
bkgoh27 <bengkoon.goh27@gmail.com>
2022-04-03 12:29:03 +0800

    check the number of digits

--- ifElseIfStatements.go
+++ ifElseIfStatements.go
@@ -5,9 +5,17 @@ import "fmt"
 func main() {
 	//Insert your code here
 	//Hint if a:= ?? ; condition {  }
-	if a := 17; a%2 == 0 {
+	a := 17
+
+	if a%2 == 0 {
 		fmt.Println("Even")
 	} else {
 		fmt.Println("Odd")
+	}
+
+	if a >= 10 {
+		fmt.Println("It has more than 1 digit!")
+	} else {
+		fmt.Println("It has only 1 digit!")
 	}
 }
