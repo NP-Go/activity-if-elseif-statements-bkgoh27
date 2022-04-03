99c659a9f00ea186718b4261c610007fc8eec332
bkgoh27 <bengkoon.goh27@gmail.com>
2022-04-03 12:26:28 +0800

    check if given number is even or odd

--- ifElseIfStatements.go
+++ ifElseIfStatements.go
@@ -1,6 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
 	//Insert your code here
 	//Hint if a:= ?? ; condition {  }
+	if a := 17; a%2 == 0 {
+		fmt.Println("Even")
+	} else {
+		fmt.Println("Odd")
+	}
 }
